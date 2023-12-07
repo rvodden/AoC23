6512ae1e6e46012947b376a73d4aaea28011b2ed
Richard Vodden <richard@humanisingautonomy.com>
2023-12-07 21:49:11 +0000

    Day 7

---
+++ day-07/src/part1.rs
@@ -0,0 +1,80 @@
+use std::ops::Deref;
+
+use itertools::Itertools;
+
+pub fn process<'a>(input: &'a str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let (hand, bid) = line.split_once(" ").unwrap();
+            (
+                hand,
+                bid.parse::<u32>().expect("should be a number"),
+                score_hand(hand),
+            )
+        })
+        .sorted_by_key(|x| x.2)
+        .enumerate()
+        // .inspect(|x| { dbg!(x);} )
+        .map(|(rank, (_, bid, _))| (rank + 1) as u32 * bid)
+        .sum()
+}
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+fn to_tuple(hand: &str) -> (u32, u32, u32, u32, u32) {
+    hand.chars()
+        .map(|c| match c {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 11,
+            'T' => 10,
+            value => value.to_digit(10).expect("should be a digit"),
+        })
+        .collect_tuple()
+        .expect("should be a tuple")
+}
+
+fn score_hand(hand: &str) -> (HandType, (u32, u32, u32, u32, u32)) {
+    use HandType::*;
+
+    let counts = hand.chars().counts();
+    let values = counts.values().sorted().join("");
+    let hand_type = match values.deref() {
+        "5" => FiveOfAKind,
+        "14" => FourOfAKind,
+        "23" => FullHouse,
+        "113" => ThreeOfAKind,
+        "122" => TwoPair,
+        "1112" => OnePair,
+        "11111" => HighCard,
+        value => panic!("should never happen. Encountered `{}`", value),
+    };
+
+    (hand_type, to_tuple(hand))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let lines = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+        let result = process(lines);
+        assert_eq!(result, 6440);
+    }
+}
